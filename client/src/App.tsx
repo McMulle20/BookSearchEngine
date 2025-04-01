e1ea90dc1d69ac48ae318b7dc3f3e8ca16b08a42
mcmul <mcmulle20.laxwi@gmail.com>
2025-04-01 13:54:42 -0500

    wrap apolloprovider in apptsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import Navbar from './components/Navbar';
+
+const cache = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
+    <ApolloProvider client={cache}>
       <Navbar />
       <Outlet />
+    </ApolloProvider>
     </>
   );
 }
